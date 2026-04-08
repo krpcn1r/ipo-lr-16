62590adfad3958d37db30c8e287aeb6f44ea3de4
Stepan <zaystep09@gmail.com>
2026-04-08 10:36:38 +0300

    LR18

--- GadgetShop/PortativeGadgetShop/settings.py
+++ GadgetShop/PortativeGadgetShop/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 LOGIN_URL = '/accounts/login/'         
 LOGIN_REDIRECT_URL = '/catalog/'        
-LOGOUT_REDIRECT_URL = '/catalog/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
