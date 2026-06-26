4689ffc91766788329c59eedd1dff78cbcf4a98c
krpcn1r <zaystep09@gmail.com>
2026-06-26 11:32:33 +0300

    Serve product media files in production (DEBUG=False)

--- GadgetShop/PortativeGadgetShop/urls.py
+++ GadgetShop/PortativeGadgetShop/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from PortativeGadgetShop import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # В продакшне (DEBUG=False) helper static() ничего не отдаёт —
+    # раздаём загруженные изображения товаров напрямую через serve.
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
